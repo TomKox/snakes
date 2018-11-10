34533ea32945e43b9bd6e9948464f6fc041ed3b5
Tom Kox <kox.tom@gmail.com>
2018-11-10 22:24:02 +0100

    improve UI.GameOver() method

--- Snakes/UI.cs
+++ Snakes/UI.cs
@@ -8,17 +8,17 @@ namespace Snakes
 {
     class UI
     {
-
         public static bool GameOver()
         {
-
             Console.ForegroundColor = ConsoleColor.Red;
             ConsoleHelper.Center("G A M E   O V E R");
             int y = Console.CursorTop += 2;
-            ConsoleHelper.Center("Play again? (Y/N) ", y);
-            string playAgain = Console.ReadLine();
+            ConsoleHelper.Center("Play again? (Y/N)", y);
+            Console.CursorVisible = true;
+            string playAgain = Console.ReadKey().KeyChar.ToString();
             if (playAgain.ToLower() == "y") return false;
-            else return true;
+            else if (playAgain.ToLower() == "n") return true;
+            else return GameOver();
         }
     }
 }
